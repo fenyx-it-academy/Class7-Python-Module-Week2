13a5ac78937f342a558dd297bace9dbb3dde6de7
Rumeysa Yakar <yakarr.ry@gmail.com>
2022-10-04 20:49:44 +0200

    created functions

--- answer2.py
+++ answer2.py
@@ -4,8 +4,10 @@ punc = """!()-[]{ };:'"\,<>./?@#$%^&*_~"""
 new_form_sentence = ' '
 dict = {}
 for i in sentence:
+    
     if i not in punc:
         new_form_sentence += i
+    
     for keys in new_form_sentence:
         keys = keys.lower()
         keys = keys.strip()
@@ -14,27 +16,3 @@ for i in sentence:
     new_dict = list(zip(dict.keys(), dict.values()))
 
 print(str(new_dict))
-
-
-
-# sentence = input('Please write a sentence: ')
-# sentence = sentence.replace(' ', '')
-# # punc = """!()-[]{ };:'"\,<>./?@#$%^&*_~"""
-# # new_form_sentence = ' '
-# # for i in sentence:
-# #     if i not in punc:
-# #         new_form_sentence += i
-# dict = {}
-# punc = """!()-[]{ };:'"\,<>./?@#$%^&*_~"""
-# new_form_sentence = ' '
-# for i in sentence:
-#     if i not in punc:
-#         new_form_sentence += i
-#     for keys in new_form_sentence:
-#         keys = keys.lower()
-#         keys = keys.strip()
-            
-#     dict[keys] = dict.get(keys,0) + 1
-#     new_dict = list(zip(dict.keys(), dict.values()))
-
-# print(str(new_dict))
---
+++ answer4.py
@@ -0,0 +1,11 @@
+# ### 4.
+# A palindromical prime number is a prime number that reads the same when reversed. 
+# Write a function which returns the nearest palindromical prime number less than the multiplication of all the arguments.
+
+# ```
+# Example
+# Input1>>>  some_function(2, 3, 4) 
+# Output1>>>  the nearest palindromical prime number less than 24
+# Input2>>> some_function(31, 77, 99)
+# Output2>>> the nearest palindromical prime number less than 236,313
+# ```
