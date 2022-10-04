96e1060e1d821d18980eba66bee65e712f43dab5
Rumeysa Yakar <yakarr.ry@gmail.com>
2022-10-04 21:42:05 +0200

    I'm not sure of the solution but wanted to try it.

--- answer4.py
+++ answer4.py
@@ -1,8 +1,3 @@
-#### 4.
-# A palindromical prime number is a prime number that reads the same when reversed. 
-# Write a function which returns the nearest palindromical prime number less than the multiplication of all the arguments.
-
-
 def some_function(a, b, c):
     mult = a * b * c
     return ('The nearest palindromical prime number less than {}'.format(mult))
@@ -13,12 +8,3 @@ c = int(input('Please enter last number: '))
 
 print (some_function(a, b, c))
 
-
-
-# ```
-# Example
-# Input1>>>  some_function(2, 3, 4) 
-# Output1>>>  the nearest palindromical prime number less than 24
-# Input2>>> some_function(31, 77, 99)
-# Output2>>> the nearest palindromical prime number less than 236,313
-# 
