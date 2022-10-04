d432370fd778c6f3de3b0ae557434a79609af821
Rumeysa Yakar <yakarr.ry@gmail.com>
2022-10-04 20:13:35 +0200

    created a function with slicing

--- answer1.py
+++ answer1.py
@@ -3,14 +3,14 @@ def rotateArray(a, d):
     if d > 0:                                          
         a[:] = a[-d:n] + a[:-d] 
             
-        # print(a)                                        
+                                                
         if d > n:                                       
             d = d % n                                   
             print(a)
        
     else:
         a[:] = a[-d:n] + a[:-d]
-        # print(a)
+        
 
     return a
 list = list(map(int,input('Enter a list?').strip().split()))
